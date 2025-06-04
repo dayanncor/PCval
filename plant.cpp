7b1fc3a7ed62e2929dd47f983b9006c2b15893b7
dyann <vcordobacorrales@gmail.com>
2025-06-04 02:06:43 -0500

    Create plant.cpp

---
+++ plant.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define forn(i, n) for (int i = 0; i < n; i++)
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+#define ll long long
+#define sz(x)    int((x).size())
+using namespace std;
+#define int int64_t
+main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+
+
+
+
+
+
+
+  return 0;
+
+}
