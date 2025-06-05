5293a12322ba96bac9f7b371b5d2f1aa5c8a4718
dyann <vcordobacorrales@gmail.com>
2025-06-04 22:06:27 -0500

    Create funcionesQueue.cpp

---
+++ Stack, Queue, Sets, Maps y Vectores/funcionesQueue.cpp
@@ -0,0 +1,30 @@
+//Tipos de queue
+queue<int>name;
+priority_queue<int>name2;
+deque<int>name;
+queue<pair<int, deque<int>>> name;
+queue<pair<int, list<int>>> name;
+
+//Funciones para queue
+name.push(x);     // Agrega al final
+name.pop();        // Elimina el primero
+name.front();      // Primer elemento
+name.back();       // ultimo elemento
+name.empty();      // Verifica si esta vacia
+name.size();       // Cantidad de elementos
+
+//Funciones para priority_queue
+name2.push(5);
+name2.push(8);
+name2.push(3);
+name2.top();       // Retorna el mayor (8)
+name2.pop();       // Elimina el mayor
+ 
+//Funciones para el deque
+name3.push_back(1);    // Al final
+name3.push_front(2);   // Al inicio
+name3.pop_back();      // Elimina al final
+name3.pop_front();     // Elimina al inicio
+name3.front();         // Primer elemento
+name3.back();          // Último elemento
+name3[0];              // Acceso directo por índice
