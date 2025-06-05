58aefed36f6498091e3f889b82afd3a6eee4fa59
dyann <vcordobacorrales@gmail.com>
2025-06-04 21:44:25 -0500

    Update FuncionesVector.cpp

--- Stack, Queue, Sets, Maps y Vectores/FuncionesVector.cpp
+++ Stack, Queue, Sets, Maps y Vectores/FuncionesVector.cpp
@@ -4,7 +4,7 @@ vector<pair<int,int>name;
  //metodos de un vector
 name.push_back(); //Insertar un elemento al final
 name.pop_back(); //elimina el ultimo elemento
-name.size(); //devuelve el tamaño del vector
+name.size(); //devuelve el tamanio del vector
 name.empty(); //Retorna true si el vector esta vacio
 name.clear(); //Elimina todo los elementos del vector
 name.at(i); //Devuelve el valor que esta en esa posicion
@@ -13,7 +13,7 @@ name.back(); //devuelve el ultimo valor
 name.insert(pos, val); // inserta un valor en una posicion espeficica
 name.erase(pos); //elimina un elemento de una posicion especifica
 name.erase(star,end); //elimina los elementos de ese rango
-name.resize(n); //cambia el tamaño del vector a n elementos
+name.resize(n); //cambia el tamanio del vector a n elementos
 name.assign(n,val); //Asigna n elementos con valor val
 name.swap(otrovec); //Intercambia el contenido con el otrovec
 begin()/end() // retorna los iteradores del inicio y final
