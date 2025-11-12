3905d5a5e8c3a810a279292d0f59e1b0ec8b3d1c
dyann <vcordobacorrales@gmail.com>
2025-11-11 23:14:55 -0500

    Update plant.cpp

--- Plantillas/plant.cpp
+++ Plantillas/plant.cpp
@@ -1,12 +1,18 @@
 #include <bits/stdc++.h>
+#include <cstdint>
 #define forn(i, n) for (int i = 0; i < n; i++)
+#define forne(i, j, n) for (int i = j; i < n; i++)
 #define all(x) x.begin(), x.end()
 #define endl '\n'
+#define f first
+#define s second
+#define pb push_back
 #define ll long long
-#define sz(x)    int((x).size())
-#define each(i,x) for(auto &&i:x)
+#define sz(x) int((x).size())
+#define each(i, x) for (auto &&i : x)
 using namespace std;
 #define int int64_t
+
 main() {
   ios_base::sync_with_stdio(false);
   cin.tie(0);
